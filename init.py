a773a6e27ebbf5ec4b7a638eafd6121f864641ad
ahmed <ahmedsaheed2@outlook.com>
2024-04-15 04:19:23 +0100

    feat(core): user login first pass

--- init.py
+++ init.py
@@ -1,1 +1,30 @@
-print("John Doe")
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import google.oauth2.id_token
+from google.auth.transport import requests
+
+
+app = FastAPI()
+
+firebase_request_adapter = requests.Request()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    id_token = request.cookies.get("token")
+    error_message = "No error here"
+    user_token = None
+
+    if id_token:
+        try:
+            user_token = google.oauth2.id_token.verify_firebase_token(
+                id_token, firebase_request_adapter
+            )
+        except ValueError as err:
+            print(str(err))
+    return templates.TemplateResponse('main.html', {'request': request, 'user_token': user_token, 'error_message': error_message})
---
+++ static/firebase-login.js
@@ -0,0 +1,90 @@
+'use strict'
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js'
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+} from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAD3KGLLWHlUkNS5E1H0YGEmnkFrOClyPk',
+    authDomain: 'griffithlabs-414212.firebaseapp.com',
+    projectId: 'griffithlabs-414212',
+    storageBucket: 'griffithlabs-414212.appspot.com',
+    messagingSenderId: '303368494173',
+    appId: '1:303368494173:web:6623f824cd71d44a7041b4',
+}
+
+window.addEventListener('load', function () {
+    const app = initializeApp(firebaseConfig)
+    const auth = getAuth(app)
+    UpdateUI(document.cookie)
+    console.log('Hello World load')
+
+    document.getElementById('sign-up').addEventListener('click', () => {
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(userCredential => {
+                const user = userCredential.user
+                console.log(user)
+                user.getIdToken().then(token => {
+                    document.cookie = `token=${token};path=/;SameSite=Strict`
+                    window.location = '/'
+                })
+            })
+            .catch(error => {
+                console.log(error.code + ' ' + error.message)
+            })
+    })
+
+    document.getElementById('login').addEventListener('click', () => {
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+        signInWithEmailAndPassword(auth, email, password)
+            .then(userCredential => {
+                const user = userCredential.user
+                console.log(user)
+                user.getIdToken().then(token => {
+                    document.cookie = `token=${token};path=/;SameSite=Strict`
+                    window.location = '/'
+                })
+            })
+            .catch(error => {
+                console.log(error.code + ' ' + error.message)
+            })
+    })
+
+    document.getElementById('sign-out').addEventListener('click', () => {
+        signOut(auth).then(() => {
+            document.cookie = 'token=;path=/;SameSite=Strict'
+            window.location = '/'
+        })
+    })
+})
+
+function UpdateUI(cookie) {
+    var token = parseCookieToken(cookie)
+    console.log('token: ' + token.length)
+    if (token.length > 0) {
+        document.getElementById('login-box').style.display = 'none'
+        document.getElementById('sign-out').style.display = 'block'
+    } else {
+        document.getElementById('login-box').style.display = 'block'
+        document.getElementById('sign-out').style.display = 'none'
+    }
+}
+
+function parseCookieToken(cookie) {
+    var strings = cookie.split(';')
+
+    for (var i = 0; i < strings.length; i++) {
+        var pair = strings[i].split('=')
+        if (pair[0] == 'token') {
+            return pair[1]
+        }
+    }
+    return ''
+}
