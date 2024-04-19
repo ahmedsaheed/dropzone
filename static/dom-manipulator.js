53a5ffeb6f013e91a5664e3bd8fd0347170896d8
ahmed <ahmedsaheed2@outlook.com>
2024-04-19 01:55:13 +0100

    feat(core):dom manipulation for deleting directories

--- static/dom-manipulator.js
+++ static/dom-manipulator.js
@@ -1,23 +1,17 @@
 window.addEventListener('load', function () {
-    const tdElements = document?.querySelectorAll('.timestamps')
-
+    var tdElements = document?.querySelectorAll('.timestamps')
     tdElements.forEach(td => {
-        const rawTimestamp = td.textContent.trim() // Get raw timestamp
-
-        const date = new Date(rawTimestamp)
-
-        const options = {
+        var rawTimestamp = td.textContent.trim()
+        var date = new Date(rawTimestamp)
+        var options = {
             year: 'numeric',
             month: 'long',
             day: 'numeric',
             hour: 'numeric',
             minute: 'numeric',
-            hour12: true, // For 12-hour AM/PM format
+            hour12: true,
         }
-
-        const formattedDateTime = date.toLocaleString('en-US', options)
-
-        td.textContent = formattedDateTime
+        td.textContent = date.toLocaleString('en-US', options)
     })
 
     const humanFileSize = size => {
@@ -29,24 +23,18 @@ window.addEventListener('load', function () {
         )
     }
 
-    const sizeElements = document?.querySelectorAll('.filesize')
-    // convert bytes to human readable format
-
+    var sizeElements = document?.querySelectorAll('.filesize')
     sizeElements.forEach(sizeElement => {
-        const rawSize = sizeElement.textContent.trim()
-
-        const size = parseInt(rawSize)
-
-        const humanReadableSize = humanFileSize(size)
-        console.log(humanReadableSize)
-        sizeElement.textContent = humanReadableSize
+        sizeElement.textContent = humanFileSize(
+            parseInt(sizeElement.textContent.trim())
+        )
     })
 
-    const inputFile = document?.getElementById('dropzone-file')
-    const fileNameDisplay = document?.getElementById('selected-file-display')
+    var inputFile = document?.getElementById('dropzone-file')
+    var fileNameDisplay = document?.getElementById('selected-file-display')
 
     inputFile?.addEventListener('change', event => {
-        const selectedFile = event.target.files[0]
+        var selectedFile = event.target.files[0]
 
         if (selectedFile) {
             document
@@ -56,14 +44,27 @@ window.addEventListener('load', function () {
                 ?.getElementById('upload-details-minor')
                 .classList.add('hidden')
 
-            const filename = selectedFile.name
-
-            const p = document.createElement('p')
+            var p = document.createElement('p')
             p.className =
                 'text-xs text-center text-gray-500 dark:text-gray-400 uppercase tracking-wider font-semibold'
-            p.textContent = filename
-
+            p.textContent = selectedFile.name
             fileNameDisplay.appendChild(p)
         }
+    })
+
+    var dirNameInput = document?.getElementById('dir_name')
+    dirNameInput?.addEventListener('input', () => {
+        var submitButton = document?.getElementById('create-folder-button')
+        dirNameInput.value.length > 0
+            ? submitButton?.removeAttribute('disabled')
+            : submitButton?.setAttribute('disabled', 'true')
+    })
+
+    var deleteButtons = document?.querySelectorAll('#wants-to-delete')
+    deleteButtons.forEach(deleteButton => {
+        deleteButton.addEventListener('click', () => {
+            document.getElementById('actively-deleted-name').textContent =
+                localStorage.getItem('wants-to-delete')
+        })
     })
 })
