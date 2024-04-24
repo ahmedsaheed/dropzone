b516633db9f76cfd14297676b60dd6533b27d14c
ahmed <ahmedsaheed2@outlook.com>
2024-04-24 22:51:11 +0100

    feat(core): check if file already exist in a dir...
    TODO: ask user if overwrite in required

--- scripts/file.py
+++ scripts/file.py
@@ -4,6 +4,13 @@ import local_constants
 project_name = local_constants.PROJECT_NAME
 project_storage_bucket = local_constants.PROJECT_STORAGE_BUCKET
 
+def does_file_exist(file, prefix, uid):
+    storage_client = storage.Client(project=project_name)
+    bucket = storage_client.bucket(project_storage_bucket)
+    path = f"users/{uid}/{prefix}{file.filename}"
+    blob = bucket.blob(path)
+    return blob.exists()
+
 def add_file(file, prefix, uid):
     storage_client = storage.Client(project=project_name)
     bucket = storage_client.bucket(project_storage_bucket)
